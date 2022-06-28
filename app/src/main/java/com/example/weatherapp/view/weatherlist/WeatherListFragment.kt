cf1d4f41833cf821d0c0d405ab8634d700937541
engi <calnikov13@gmail.com>
2022-06-28 12:58:21 +0300

    Создана связь между фрагментом и ViewModel

--- app/src/main/java/com/example/weatherapp/view/weatherlist/WeatherListFragment.kt
+++ app/src/main/java/com/example/weatherapp/view/weatherlist/WeatherListFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.databinding.FragmentWeatherListBinding
+import com.example.weatherapp.viewmodel.AppState
 
 class WeatherListFragment : Fragment() {
 
@@ -29,7 +32,31 @@ class WeatherListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(WeatherListViewModel::class.java)
+        viewModel.getData().observe(viewLifecycleOwner, object : Observer<AppState>{
+            override fun onChanged(data: AppState) {
+                renderData(data)
+            }
+        })
+        viewModel.sendRequest()
+    }
 
+    private fun renderData(appState: AppState){
+        when(appState){
+            is AppState.Error -> {
+                Toast.makeText(getActivity(),"Произошла ошибка при получений данных!",Toast.LENGTH_SHORT).show();
+            }
+            AppState.Loading -> {
+                binding.loading.visibility = View.VISIBLE
+            }
+            is AppState.Success -> {
+                val data = appState.weatherData
+
+                binding.city.text = data.city.name
+                binding.temperatureValue.text = data.temperature.toString()
+                binding.feelsLikeValue.text = data.feelsLike.toString()
+                binding.coordinates.text = "${data.city.lat}/${data.city.lon}"
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/weatherapp/view/weatherlist/WeatherListViewModel.kt
+++ app/src/main/java/com/example/weatherapp/view/weatherlist/WeatherListViewModel.kt
@@ -3,6 +3,8 @@ package com.example.weatherapp.view.weatherlist
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.weatherapp.model.Repository
+import com.example.weatherapp.model.RepositoryLocalImpl
+import com.example.weatherapp.model.RepositoryRemoteImpl
 import com.example.weatherapp.viewmodel.AppState
 
 class WeatherListViewModel : ViewModel() {
@@ -11,6 +13,34 @@ class WeatherListViewModel : ViewModel() {
 
     lateinit var repository : Repository
 
+    fun getData():MutableLiveData<AppState>{
+        choiceRepository()
+        return liveData
+    }
 
+    private fun choiceRepository(){
+        repository = if(isConnection()){
+            RepositoryRemoteImpl()
+        }else{
+            RepositoryLocalImpl()
+        }
+    }
+
+
+    fun sendRequest(){
+        liveData.value = AppState.Loading
+        val randomNum = (0..3).random()
+
+        if(randomNum == 1){
+            liveData.postValue(AppState.Error(throw IllegalStateException("Ошибка получения данных")))
+        }
+        else{
+            liveData.postValue(AppState.Success(repository.getWeather(55.755826, 37.617299900000035)))
+        }
+    }
+
+    private fun isConnection(): Boolean {
+        return false
+    }
 
 }
